7a3ce941461097b11d7e68e82da569d4310e9008
xzb666-hub <1035038958@qq.com>
2024-02-04 22:32:20 +0800

    Site updated: 2024-02-04 22:32:19

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2023/09/18/Burpsuite的使用/","2024/01/01/C语言/","2023/09/18/HTTP协议/","2023/09/18/ROIS 冬训题解/","2023/09/18/JS/","2023/09/18/MYSQL提权/","2023/09/18/Linux基本命令/","2023/09/18/The Missing Semester/","2023/09/18/鲲鹏基座-跨平台软件转移/","2024/01/01/Node.js/","2023/10/28/Web/","2023/09/18/SQL/"];function toRandomPost(){
+var posts=["2024/01/01/C语言/","2023/09/18/JS/","2024/01/01/Node.js/","2023/09/18/MYSQL提权/","2023/09/18/The Missing Semester/","2023/09/18/Linux基础/","2023/09/18/ROIS 冬训题解/","2023/09/18/网安基础/","2023/09/18/鲲鹏基座-跨平台软件转移/","2023/09/18/SQL/","2023/10/28/Web开发入门/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
